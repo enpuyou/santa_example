c1024f324af29e2cbeaa8e0365b44898b86bd188
enpuyou <youe2@allegheny.edu>
2019-04-23 21:03:20 -0400

    Add example for the new generate_st function

--- test_simpleadd.py
+++ test_simpleadd.py
@@ -14,15 +14,22 @@ import simpleadd
 # use * to pass in the given decorator
 # specify how many parameters needed in the test case like below
 
-type_add_output = util.get_input_type(
+util.set_path("monkeytype.sqlite3")
+
+# type_add_output = util.get_input_type(
+#     function="add",
+#     module="simpleadd",
+# )
+# list_type = util.read_type(type_add_output)
+# func_st = util.create_st(list_type)
+
+test_st = util.generate_st(
     function="add",
     module="simpleadd",
 )
-list_type = util.read_type(type_add_output)
-func_st = util.create_st(list_type)
 
 
-@given(*func_st)
+@given(*test_st)
 def test_add(a, b):
     """ use tool to test add function """
     assert simpleadd.add(a, b) == a + b
